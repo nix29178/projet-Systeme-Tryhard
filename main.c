ac39e604fb1043fade861857d695acb52dc585e6
Nicolas <nicolas.lbs@gmail.com>
2015-05-30 23:57:37 +0200

    ajout de touch.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "blocks.h"
+#include "touch.c"
+#include <string.h>
 
 
 int main(){
 	superBlock *sb = initSGF();
 	creaDir(sb,1,"test1");
 	creaDir(sb,1,"test2");
-	creerFicher(sb,1,"testDeFichier");
-	
-	modifContenu(sb,4, "Bonjour je m'appelle Nicolas Le Bourhis et j'atteste que Cosmin est le +++ beau gosse de la terre");
-	char *test = lireFichier(sb,4);
-	printf("\naffichage variable : ---- %s\n",test);
+	//creerFicher(sb,1,"testDeFichier");
+	touch(sb,1,"testDeFichier");
+	touch(sb,1,"testDeFichier");
 	
 	return 0;
 }
---
+++ touch.c
@@ -0,0 +1,20 @@
+#include "blocks.h"
+
+touch(superBlock *sb, int argInode, char *nomFichier){
+	 
+	 blockD *tmpD=noInodeToBlockD(sb,argInode);
+	 int ok=1,i=2;
+	 while(ok==1 && tmpD->inodes[i]!=0){
+		 if(strcmp(nomFichier, tmpD->sousDirect[i])==0){
+			 ok=0;
+		 }
+		 i++;
+	 }
+	 if(ok==1){
+	     creerFicher(sb, argInode, nomFichier);
+	     printf("fichier %s créé\n",nomFichier);
+	 }
+	 else{
+		 printf("un fichier ou dossier avec ce nom existe deja\n");
+     }
+}
