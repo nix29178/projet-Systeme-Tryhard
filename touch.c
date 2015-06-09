42c03526960e679638e8fd4fb3780d2dd8a9df02
Nicolas <nicolas.lbs@gmail.com>
2015-06-09 16:12:45 +0200

    modif sur touch

--- touch.c
+++ touch.c
@@ -3,21 +3,27 @@
 void touch(superBlock *sb, int argInode, char *nomFichier){
 	 
 	 blockD *tmpD=noInodeToBlockD(sb,argInode);
-	 int ok=1,i=2;
-	 while(ok==1 && tmpD->inodes[i]!=0){
-		 if(strcmp(nomFichier, tmpD->sousDirect[i])==0){
+	 int ok=1,i=2,vide=0;
+	 for(i;i<20;i++){
+		 if(tmpD->inodes[i]!=0 && strcmp(nomFichier, tmpD->sousDirect[i])==0){
 			 ok=0;
+			 break;
 		 }
-		 i++;
 	 }
-	 if(i==20){
+	 for(i=2;i<20;i++){
+		if(tmpD->inodes[i]==0){
+			vide=i;
+			break;
+		}
+	 }
+	 if(vide==0){
 		printf("dossier plein\n");
 	 }
-	 else if(ok==1){
+	 else if(ok==0){
+		 printf("un fichier ou dossier avec ce nom existe deja\n");
+     }
+     else{
 	     creerFicher(sb, argInode, nomFichier);
 	     printf("fichier %s créé\n",nomFichier);
 	 }
-	 else{
-		 printf("un fichier ou dossier avec ce nom existe deja\n");
-     }
 }
